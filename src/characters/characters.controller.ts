4958b5142e8972b589c989ea7b5c4807716eb200
RoockDev <zar0_31@outlook.com>
2026-02-18 00:00:22 +0100

    feat: cliente para probar

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { CharactersService } from './characters.service';
-import { AttackCharacterDto } from './dto/attack-character.dto';
 import { JwtAuthGuard } from '../auth/jwt.strategy/jwt-auth.guard';
 import { Roles } from '../auth/roles/roles.decorator';
 import { RolesGuard } from '../auth/roles/roles.guard';
@@ -14,17 +13,5 @@ export class CharactersController {
   @Roles('USER', 'ADMIN')
   findAll() {
     return this.characterService.findAll();
-  }
-
-  @Post('attack')
-  @Roles('USER', 'ADMIN')
-  attack(@Body() attackCharacterDto: AttackCharacterDto) {
-    return this.characterService.attack(attackCharacterDto);
-  }
-
-  @Post('reset')
-  @Roles('ADMIN')
-  reset() {
-    return this.characterService.reset();
   }
 }
